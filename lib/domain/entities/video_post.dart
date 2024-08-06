994e27af6b0988c14bc8004c1a1261031e3e1e91
Williamparededl1 <wparedesl1995@gmail.com>
2024-08-06 12:30:33 -0500

    Initial commit

---
+++ lib/domain/entities/video_post.dart
@@ -0,0 +1,12 @@
+class VideoPost {
+  final String caption;
+  final String videourl;
+  final int likes;
+  final int views;
+
+  VideoPost(
+      {required this.caption,
+      required this.videourl,
+      this.likes = 0,
+      this.views = 0});
+}
---
+++ lib/infrastructure/repositories/video_posts_repository_impl.dart
@@ -0,0 +1,20 @@
+import 'package:toktik/domain/datasources/video_post_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+
+class VideoPostsRepositoryImpl implements VideoPostRepository {
+  final VideoPostDatasource videoPostDatasource;
+
+  VideoPostsRepositoryImpl({required this.videoPostDatasource});
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByPage(String userID) {
+    // TODO: implement getFavoriteVideosByPage
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videoPostDatasource.getTrendingVideosByPage(page);
+  }
+}
